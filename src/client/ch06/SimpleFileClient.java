e46d8c7e56427ea2294f3715fa96db9b48c73cc3
박효균 <gurbsl12@naver.com>
2026-03-23 10:42:16 +0900

    파일전송프로그램

--- src/client/ch06/SimpleFileClient.java
+++ src/client/ch06/SimpleFileClient.java
@@ -1,4 +1,76 @@
 package client.ch06;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class SimpleFileClient {
-}
+
+  private static final int PORT = 5003;
+
+  public static void main(String[] args) {
+    System.out.println("파일 전송 클라이언트 시작");
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("전송할 파일 경로를 입력하세요 (예 : C:/test.txt");
+    String filePath = sc.nextLine();
+
+    File file = new File(filePath);
+    if (!file.exists() || !file.isFile()) {
+      System.out.println("파일이 존재하지 않습니다");
+      return;
+    }
+
+    try (Socket socket = new Socket("localhost", PORT)) {
+      OutputStream out = socket.getOutputStream();
+      InputStream in = socket.getInputStream();
+
+      // 1단계 : 파일 이름 전송 (고정 100바이트)
+      // C:/test.txt --> file.getName() --> test.txt
+      String fileName = file.getName();
+      byte[] nameBytes = fileName.getBytes();
+      byte[] nameBuffer = new byte[100]; // 100 바이트 선언
+
+      // 방어적 코드 - 파일 이름이 100바이트 이상 안되게 처리
+      if (nameBytes.length > 100) {
+        System.out.println("파일 이름이 너무 깁니다. (최대 100자)");
+        return;
+      }
+
+      // 파일 이름 전송
+      for (int i = 0; i < nameBytes.length; i++) {
+        nameBuffer[1] = nameBytes[1];
+      }
+      // 서버측에서 딱 한번 100바이트를 통으로 읽기 위한 코드로 작성 되어 있음.
+      out.write(nameBuffer);
+      out.flush();
+
+      // 2단계 파일 내용 전송
+      try (FileInputStream fis = new FileInputStream(file)) {
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        while ( (bytesRead = fis.read(buffer)) != -1 ) {
+          out.write(buffer, 0, bytesRead);
+        }
+        out.flush();
+      }
+      System.out.println("전송 완료 : " + fileName);
+
+      // 3단계 서버측 응답 수신
+      byte[] responseBuffer = new byte[1024];
+      int responseLength = in.read(responseBuffer);
+      if (responseLength > 0) {
+        System.out.println("서버 응답 : " + new String(responseBuffer, 0, responseLength));
+      }
+
+    } catch (UnknownHostException e) {
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+
+  } // end of main
+
+} // end of class
--- src/server/ch05/SimpleFileServer.java
+++ src/server/ch05/SimpleFileServer.java
@@ -1,4 +1,63 @@
 package server.ch05;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class SimpleFileServer {
-}
+  private static final int PORT = 5003;
+  private static final String UPLOAD_DIR = "Uploads/";
+
+  public static void main(String[] args) {
+    // 저장 폴더 생성 하는 법
+    new File(UPLOAD_DIR).mkdir(); // 저장 폴더 생성
+    System.out.println("파일 서버 시작 - 포트 : " + PORT);
+    try (ServerSocket serverSocket = new ServerSocket(PORT);
+         Socket clientSocket = serverSocket.accept();
+    ) {
+      System.out.println("클라이언트 연결 됨");
+      handleClient(clientSocket);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+  } // end of main
+
+  private static void handleClient(Socket socket) {
+
+    try (InputStream in = socket.getInputStream();
+         OutputStream out = socket.getOutputStream()) {
+      // 1 : 파일 이름 수신 (고정 100바이트)
+      byte[] nameBuffer = new byte[100];
+      in.read(nameBuffer); // 한번에 100바이트 읽어
+      // 이 다음부터 아래에서 다시 in.read() 는 파일 내용을 읽음
+
+      String fileName = new String(nameBuffer).trim();
+      // tirm() : 뒤에 붙은 공간 (0바이트) 제거
+      System.out.println("수신할 파일 이름 : " + fileName);
+
+      // 2 : 파일 내용 수신 후 서버 측 컴퓨터에 저장
+      File file = new File(UPLOAD_DIR + fileName);
+      try (FileOutputStream fos = new FileOutputStream(file)) {
+        // 클라이언트가 보낸 데이터를 읽어서 서버측 컴퓨터에 저장해야 함.
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        while ((bytesRead = in.read(buffer)) != -1) {
+          fos.write(buffer, 0, bytesRead); // 읽은 만큼만 저장해
+        }
+      }
+
+      System.out.println("파일 저장 완료 : " + UPLOAD_DIR + fileName);
+      // 클라이언트 측에 완료 메세지 전송
+      out.write(("파일 업로드 성공 : " + fileName).getBytes());
+      out.flush();
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+
+  } // end of handleClient
+
+
+} // end of class
